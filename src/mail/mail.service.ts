a57da5dd1e71373a495712a1afe24932890051f5
Aylaataydir <aylaataydir@gmail.com>
2026-07-20 21:08:01 +0200

    refactor(mail): fix mail.service content

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: Number(process.env.SMTP_PORT),
   secure: false,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   },
 });
 
