6b7468287fcdf44205a1ccc8306b5d76ff6671b0
Elif Yuzer <elifyuzer12@gmail.com>
2026-08-01 20:33:18 +0200

    chore(notificaitons) :add notificaitonRoute refs:KTZ-63

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import userRoute from "./user/userRoute.js";
 import authRoute from "./auth/authRoute.js";
-import adminRoute from './admin/index.js';
-import eventCategoryRoute from './user/eventCategoyRoute.js';
-import eventRoute from './user/eventRoute.js';
+import adminRoute from "./admin/index.js";
+import eventCategoryRoute from "./user/eventCategoyRoute.js";
+import eventRoute from "./user/eventRoute.js";
 import organizerApplicationRoute from "./user/organizerApplicationRoute.js";
 import institutionRoute from "./user/institutionRoute.js";
+import notificationRoute from "./user/notificationRoute.js";
 
 const router = Router();
 
@@ -17,9 +18,6 @@ router.use("/category", eventCategoryRoute);
 router.use("/events", eventRoute);
 router.use("/organizer-applications", organizerApplicationRoute);
 router.use("/institutions", institutionRoute);
+router.use("/notifications", notificationRoute);
 
 export default router;
-
-
-
-
