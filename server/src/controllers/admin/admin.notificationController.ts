e169ddf41ce740d086a3a3269ded723f44bcb4a8
Elif Yuzer <elifyuzer12@gmail.com>
2026-08-02 00:32:40 +0200

    feat(notification) Add getAdminNotificationQueue function for organizer_application and event_review notificationsrefs:KTZ-56

---
+++ server/src/controllers/admin/admin.notificationController.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import { Notification } from "../../models/notificationModel.js";
+import { toAdminNotificationDTO } from "../../helpers/toNotificationDTO.js";
+
+export const getAdminNotificationQueue = async (
+  req: Request,
+  res: Response,
+) => {
+  const customFilter: any = {
+    type: { $in: ["organizer_application", "event_review"] },
+  };
+
+  if (req.query.isRead === "false") {
+    customFilter.isRead = false;
+  }
+  //console.log("kontrol et filtreyi",customFilter)
+  const notifications = await res.getModelList!(Notification, customFilter);
+  const details = await res.getModelListDetails!(Notification, customFilter);
+  /*   const result = toAdminNotificationDTO(notifications); */
+  //console.log("verinin sanitizasyonuna bak",result)
+
+  res.status(200).json({
+    success: true,
+    details,
+    notifications: toAdminNotificationDTO(notifications),
+  });
+};
