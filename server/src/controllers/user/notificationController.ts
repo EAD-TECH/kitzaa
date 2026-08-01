280590cd5764a058e176e28ca89302cae859dd19
Elif Yuzer <elifyuzer12@gmail.com>
2026-08-01 18:02:33 +0200

    refactor(notifications): use toNotificationDTO in list and patch functions
    - Update list function to return DTO format
    - Update patch function to return DTO format
    
    Refs: KTZ-63

--- server/src/controllers/user/notificationController.ts
+++ server/src/controllers/user/notificationController.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express";
 import { Notification } from "../../models/notificationModel.js";
+import { _discriminatedUnion } from "zod/v4/core";
+import { toNotificationDTO } from "../../helpers/toNotificationDTO.js";
 
 export const listNotificationsById = async (req: Request, res: Response) => {
   // 1. Sadece bu kullanıcıya ait olanları filtrele
@@ -20,6 +22,64 @@ export const listNotificationsById = async (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     details,
-    data: { payload: notifications },
+    notifications:toNotificationDTO(notifications),
+  });
+};
+
+export const getUnReadNotificationCount = async (
+  req: Request,
+  res: Response,
+) => {
+  const unreadCount = await Notification.countDocuments({
+    recipientId: req.user._id,
+    isRead: false,
+  });
+
+  /* sayıyı dondum */
+  res.status(200).json({
+    success: true,
+    data: { count: unreadCount },
+  });
+};
+
+/* tek bıldırm okundu yapmak ısterse */
+
+export const patchNotification = async (req: Request, res: Response) => {
+  const notificationId = req.params.id;
+  const recipientId = req.user?._id;
+
+  const updatedAsREadnotification = await Notification.findOneAndUpdate(
+    { recipientId, _id: notificationId },
+    { isRead: true },
+    { new: true, runValidators: true },
+  );
+
+  if (!updatedAsREadnotification) {
+    throw new Error("Notification couldnt found ");
+  }
+  return res.status(200).json({
+    success: true,
+    message: "Notification is read successfully",
+   updatedAsREadnotification:toNotificationDTO(updatedAsREadnotification)
+  });
+};
+
+/* kullanıcı eger tum bıldırımlerını okundu yapmak ısterse */
+
+export const patchAllNotificationAsRead = async (
+  req: Request,
+  res: Response,
+) => {
+  /* Sadece okunmamış olanları hedefliyorum o yuzden fılterdan gecırıp false ları getırdım */
+  const customFilter: any = { recipientId: req.user._id, isRead: false };
+
+  const updatedAllnotifications = await Notification.updateMany(customFilter, {
+    isRead: true,
+  });
+
+  return res.status(200).json({
+    success: true,
+    message: "All Notifications are read successfully",
+    data: updatedAllnotifications.modifiedCount,
   });
 };
