b3579dfff4b6501f28056712cbd142a37c77beac
ELİF YÜZER <232604280+elif-yuzer@users.noreply.github.com>
2026-08-04 00:26:54 +0200

    Update admin.notificationController.ts
    fix: standardize admin notification response format

--- server/src/controllers/admin/admin.notificationController.ts
+++ server/src/controllers/admin/admin.notificationController.ts
@@ -39,7 +39,7 @@ export const getAdminNotificationQueue = async (
   //console.log("verinin sanitizasyonuna bak",result)
 
   res.status(200).json({
-    success: true,
+   error:false,
     details,
     notifications: toAdminNotificationDTO(notifications),
   });
