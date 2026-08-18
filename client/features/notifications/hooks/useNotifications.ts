ecde7614a37112e633f5dab3d12109834ff88ed7
Elif Yuzer <elifyuzer12@gmail.com>
2026-08-18 23:13:20 +0200

    feat(notifications) : add useUnRead hook for fetching isRead:false notifications
    refs:KTZ-138

---
+++ client/features/notifications/hooks/useNotifications.ts
@@ -0,0 +1,17 @@
+import { useAuthStore } from "@/features/auth/store/authStore";
+import { useQuery } from "@tanstack/react-query";
+import { listNotifications } from "../api";
+
+export const useNotifications = () => {
+  const isReady = useAuthStore((state) => state.isReady);
+  const accessToken = useAuthStore((state) => state.accessToken);
+
+  /* console.log("hazırmı",isReady)
+  console.log("token varmı",accessToken) */
+
+  return useQuery({
+    queryKey: ["notifications", "list"],
+    queryFn: () => listNotifications({ isRead: false }),
+    enabled: isReady && !!accessToken,
+  });
+};
